60d9b595e3867b3c084cbf6bb022eaa713bb512e
Wireman <zwireman@UMICH.EDU>
2023-03-09 14:24:40 -0500

    first commit

---
+++ CircleAreaCalculator.java
@@ -0,0 +1,7 @@
+public class CircleAreaCalculator {
+    public static void main(String[] args) {
+        double radius = 5.0; // radius of the circle
+        double area = Math.PI * radius * radius; // formula for area of a circle
+        System.out.println("The area of the circle is " + area);
+    }
+}
